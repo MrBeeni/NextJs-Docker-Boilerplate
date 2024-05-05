2f22cf7b4237fceddac68322e6a1ae9749d1afd5
MrBeeni <mubeen4429@gmail.com>
2024-05-05 09:57:02 +0500

    Initial setup: complete starter code

---
+++ lib/action.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import dbConnect from "./database/connect";
+import Task from "./database/task.model";
+import { useRouter } from "next/navigation";
+
+export async function getTasks() {
+  await dbConnect();
+
+  try {
+    const tasks = await Task.find({});
+    return tasks;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function createTask(params: {
+  task: string;
+  description: string;
+  status: string;
+}) {
+  await dbConnect();
+
+  const { task, description, status } = params;
+
+  try {
+    const newTask = await Task.create({ task, description, status });
+
+    return newTask;
+  } catch (err) {
+    console.log(err);
+  }
+}
